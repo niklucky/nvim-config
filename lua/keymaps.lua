accfaf31760fb05b2d2475f37860e49c0a666001
Nik <iam@niklucky.com>
2024-02-07 17:55:41 +0300

    Added copilot, tabs remaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,50 +5,50 @@ local keymap = vim.keymap -- for conciseness
 ---------------------
 
 -- use jk to exit insert mode
-keymap.set('i', 'jk', '<ESC>')
+keymap.set("i", "jk", "<ESC>")
 
 -- clear search highlights
-keymap.set('n', '<leader>nh', ':nohl<CR>')
+keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- select all
-vim.keymap.set('n', '<C-a>', 'ggVG')
+vim.keymap.set("n", "<C-a>", "ggVG")
 
 -- delete single character without copying into register
-keymap.set('n', 'x', '"_x')
-keymap.set('n', '<leader>p', '"_dP')
-keymap.set('n', '<C-a>', 'gg0VG')
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<leader>p", '"_dP')
+keymap.set("n", "<C-a>", "gg0VG")
 
 -- window management
-keymap.set('n', '<leader>wv', '<C-w>v') -- split window vertically
-keymap.set('n', '<leader>wh', '<C-w>s') -- split window horizontally
+keymap.set("n", "<leader>wv", "<C-w>v")        -- split window vertically
+keymap.set("n", "<leader>wh", "<C-w>s")        -- split window horizontally
 -- keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set('n', '<leader>wx', ':close<CR>') -- close current split window
+keymap.set("n", "<leader>wx", ":close<CR>")    -- close current split window
 
-keymap.set('n', '<leader>to', ':tabnew<CR>') -- open new tab
-keymap.set('n', '<leader>tx', ':tabclose<CR>') -- close current tab
-keymap.set('n', '<leader>tn', ':tabn<CR>') --  go to next tab
-keymap.set('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+-- keymap.set('n', '<leader>tn', ':tabn<CR>') --  go to next tab
+-- keymap.set('n', '<leader>tp', ':tabp<CR>') --  go to previous tab
+
+keymap.set("n", "<tab>", ":tabn<CR>")   --  go to next tab
+keymap.set("n", "<S-tab>", ":tabp<CR>") --  go to previous tab
 
 ----------------------
 -- Plugin Keybinds
 ----------------------
 
 -- vim-maximizer
-keymap.set('n', '<leader>sm', ':MaximizerToggle<CR>') -- toggle split window maximization
-
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
-
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
