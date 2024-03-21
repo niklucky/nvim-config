9a0e67c680c7968fd95a8350489f91fc2c39410d
Nik <iam@niklucky.com>
2024-03-21 15:34:45 +0300

    Neo tree updates for hidden files, playing around with prettier

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return   {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
@@ -8,9 +8,17 @@ return   {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set('n', '<leader>fe', ':Neotree<CR>')
-    vim.keymap.set('n', '<C-n>', ':Neotree<CR>')
-  end
+    vim.keymap.set("n", "<leader>fe", ":Neotree<CR>")
+    vim.keymap.set("n", "<C-n>", ":Neotree<CR>")
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+    })
+  end,
 }
-
-
