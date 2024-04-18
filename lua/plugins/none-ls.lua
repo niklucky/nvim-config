51d73050f70a7223b805cff8bfc9f6539ce25ed9
Nik <iam@niklucky.com>
2024-04-18 19:30:50 +0300

    Plugins updated, fixed none-ls breaking changes after update

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -12,7 +15,8 @@ return {
 				-- null_ls.builtins.formatting.prettierd,
 				null_ls.builtins.formatting.gofumpt,
 				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.diagnostics.eslint_d,
+        require("none-ls.diagnostics.eslint_d"),
+				-- none_ls.diagnostics.eslint_d,
 				-- null_ls.builtins.completion.spell,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
