ec76a5092fb22bf1628293af51f9d81fac5fc578
Nik <iam@niklucky.com>
2024-04-21 03:42:40 +0300

    Changed quick fix for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,7 +36,8 @@ return {
 							["<C-d>"] = false,
 							["<C-k>"] = actions.move_selection_previous, -- move to prev result
 							["<C-j>"] = actions.move_selection_next, -- move to next result
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+							["<C-qs>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+							["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist, -- send selected to quickfixlist
 						},
 					},
 				},
